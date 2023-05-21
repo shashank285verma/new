fe6769384c75aff55cae405b8593aa87b8f28eea
shashank285verma <134156063+shashank285verma@users.noreply.github.com>
2023-05-21 21:44:52 +0530

    Delete Desktop directory

--- Desktop/cpp/diff.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-
-int main(){
-    ll a=1,b=1;
-    cout<<(a-b)<<endl;
-}
--- Desktop/cpp/git.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-
-int main(){
-    ll a=1,b=2;
-    cout<<(a+b)<<endl;
-}
